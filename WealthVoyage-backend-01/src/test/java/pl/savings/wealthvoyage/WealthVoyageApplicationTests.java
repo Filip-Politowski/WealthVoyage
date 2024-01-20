3ee1c862cd4d11c1b7fb8ee2737d8b453b5b44d0
Filip Politowski <fil.politowski@gmail.com>
2024-01-20 20:41:31 +0100

    initial commit

---
+++ WealthVoyage-backend-01/src/test/java/pl/savings/wealthvoyage/WealthVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.savings.wealthvoyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WealthVoyageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
