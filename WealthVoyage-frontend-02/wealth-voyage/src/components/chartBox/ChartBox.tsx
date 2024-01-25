bbce7b83df95606073913b5678c474f339146d8b
Filip Politowski <fil.politowski@gmail.com>
2024-01-25 21:10:13 +0100

    created sample of line charts, added library recharts, added style to charts

---
+++ WealthVoyage-frontend-02/wealth-voyage/src/components/chartBox/ChartBox.tsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import "./chartBox.scss"
+import { Link } from 'react-router-dom';
+import { Line, LineChart, ResponsiveContainer, Tooltip } from 'recharts';
+
+const ChartBox = () => {
+    const data = [
+      {
+        name: "Page A",
+        uv: 4000,
+        pv: 2400,
+        amt: 2400,
+      },
+      {
+        name: "Page B",
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+      },
+      {
+        name: "Page C",
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+      },
+      {
+        name: "Page D",
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+      },
+      {
+        name: "Page E",
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+      },
+      {
+        name: "Page F",
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+      },
+      {
+        name: "Page G",
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+      },
+    ];
+  return (
+    <div className="chartBox">
+      <div className="boxInfo">
+        <div className="title">
+          <img src="/user.svg" alt="" />
+          <span>Savings</span>
+        </div>
+        <h1>6789.00 zł</h1>
+        <Link to="/">View All</Link>
+      </div>
+      <div className="chartInfo">
+        <div className="chart">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart width={300} height={100} data={data}>
+              <Tooltip 
+              contentStyle={{background:"transparent", border:"none", }}
+              labelStyle={{display:"none"}}
+              position={{x:10, y:80}}
+              />
+              <Line
+                type="monotone"
+                dataKey="pv"
+                stroke="#8884d8"
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="texts">
+          <span className="percentage">45%</span>
+          <span className="duration">this month</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ChartBox
--- WealthVoyage-frontend-02/wealth-voyage/src/pages/home/Home.tsx
+++ WealthVoyage-frontend-02/wealth-voyage/src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./home.scss";
 import TopBox from "../../components/topBox/TopBox";
+import ChartBox from "../../components/chartBox/ChartBox";
 
 const Home = () => {
   return (
@@ -8,11 +9,11 @@ const Home = () => {
       <div className="box box1">
         <TopBox />
       </div>
-      <div className="box box2">Box2</div>
-      <div className="box box3">Box3</div>
+      <div className="box box2"><ChartBox /></div>
+      <div className="box box3"><ChartBox /></div>
       <div className="box box4">Box4</div>
-      <div className="box box5">Box5</div>
-      <div className="box box6">Box6</div>
+      <div className="box box5"><ChartBox /></div>
+      <div className="box box6"><ChartBox /></div>
       <div className="box box7">Box7</div>
       <div className="box box8">Box8</div>
       <div className="box box9">Box9</div>
