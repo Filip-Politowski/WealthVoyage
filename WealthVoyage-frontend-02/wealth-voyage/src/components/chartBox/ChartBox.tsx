fb82c34a67ffe733737fc7b438b903ca4c7a3a4b
Filip Politowski <fil.politowski@gmail.com>
2024-01-25 21:24:37 +0100

    added svg to savings and style to it

--- WealthVoyage-frontend-02/wealth-voyage/src/components/chartBox/ChartBox.tsx
+++ WealthVoyage-frontend-02/wealth-voyage/src/components/chartBox/ChartBox.tsx
@@ -1,58 +1,58 @@
-import React from 'react'
-import "./chartBox.scss"
-import { Link } from 'react-router-dom';
-import { Line, LineChart, ResponsiveContainer, Tooltip } from 'recharts';
+import React from "react";
+import "./chartBox.scss";
+import { Link } from "react-router-dom";
+import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
 
 const ChartBox = () => {
-    const data = [
-      {
-        name: "Page A",
-        uv: 4000,
-        pv: 2400,
-        amt: 2400,
-      },
-      {
-        name: "Page B",
-        uv: 3000,
-        pv: 1398,
-        amt: 2210,
-      },
-      {
-        name: "Page C",
-        uv: 2000,
-        pv: 9800,
-        amt: 2290,
-      },
-      {
-        name: "Page D",
-        uv: 2780,
-        pv: 3908,
-        amt: 2000,
-      },
-      {
-        name: "Page E",
-        uv: 1890,
-        pv: 4800,
-        amt: 2181,
-      },
-      {
-        name: "Page F",
-        uv: 2390,
-        pv: 3800,
-        amt: 2500,
-      },
-      {
-        name: "Page G",
-        uv: 3490,
-        pv: 4300,
-        amt: 2100,
-      },
-    ];
+  const data = [
+    {
+      name: "Page A",
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: "Page B",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "Page C",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "Page D",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "Page E",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "Page F",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "Page G",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
   return (
     <div className="chartBox">
       <div className="boxInfo">
         <div className="title">
-          <img src="/user.svg" alt="" />
+          <img src="/savings.svg" alt="" />
           <span>Savings</span>
         </div>
         <h1>6789.00 zł</h1>
@@ -62,10 +62,10 @@ const ChartBox = () => {
         <div className="chart">
           <ResponsiveContainer width="100%" height="100%">
             <LineChart width={300} height={100} data={data}>
-              <Tooltip 
-              contentStyle={{background:"transparent", border:"none", }}
-              labelStyle={{display:"none"}}
-              position={{x:10, y:80}}
+              <Tooltip
+                contentStyle={{ background: "transparent", border: "none" }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: 80 }}
               />
               <Line
                 type="monotone"
@@ -84,6 +84,6 @@ const ChartBox = () => {
       </div>
     </div>
   );
-}
+};
 
-export default ChartBox
+export default ChartBox;
