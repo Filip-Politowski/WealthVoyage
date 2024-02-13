0f857126a9250a7216ac0e6d70314ecbe7adfd05
Filip Politowski <fil.politowski@gmail.com>
2024-02-13 22:07:07 +0100

    Added changing profile img

--- WealthVoyage-frontend-02/wealth-voyage/src/pages/profile/Profile.tsx
+++ WealthVoyage-frontend-02/wealth-voyage/src/pages/profile/Profile.tsx
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./profile.scss";
 import TopBox from "../../components/topBox/TopBox";
+
 const Profile = () => {
+  const [profileImage, setProfileImage] = useState<any>(
+    "https://avatars.githubusercontent.com/u/117862850?v=4"
+  );
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const base64ConversionForImages = async (e: any) => {
+    if (e.target.files[0]) {
+      getBase64(e.target.files[0]);
+    }
+  };
+
+  const getBase64 = (file: File) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+      setProfileImage(reader.result);
+    };
+    reader.onerror = function (error) {
+      console.log("Error", error);
+    };
+  };
+
+  const handleImageClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
   return (
     <div className="profile">
       <div className="profileInformation">
         <div className="box1">
           <img
-            src="https://avatars.githubusercontent.com/u/117862850?v=4"
+            src={profileImage}
             alt="profile"
+            onClick={handleImageClick}
           />
           <h1>Filip</h1>
+          <input
+            type="file"
+            onChange={(e) => base64ConversionForImages(e)}
+            ref={fileInputRef}
+          />
         </div>
         <div className="box2">
           <div className="balance">
