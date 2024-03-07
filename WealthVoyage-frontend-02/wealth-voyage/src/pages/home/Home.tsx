9f5e6e1d320b429da0021d26aea78911c0b4b8f9
Filip Politowski <fil.politowski@gmail.com>
2024-03-07 18:55:59 +0100

    Completed landing  page

--- WealthVoyage-frontend-02/wealth-voyage/src/pages/home/Home.tsx
+++ WealthVoyage-frontend-02/wealth-voyage/src/pages/home/Home.tsx
@@ -10,27 +10,43 @@ const Home = () => {
       <div className="navigation">
         <h1>Wealth Voyage</h1>
 
-        <Link to="/auth/register">
+        <Link to="/auth/signin">
           <button>Sign In</button>
         </Link>
       </div>
       <div className="homeContainer">
         <div className="welcomeText">
-          <h1>Manage Your Finances with Our Help!</h1>
+          <h1>Manage Your Finances with Our Help !</h1>
           <p>
             Achieve Stability and Profitability Our platform offers
             comprehensive financial management solutions. Whether you you need
             support in planning your personal budget, or you want to optimize
             your company's finances, we are here to help you help.
           </p>
-          <Link to="/auth/signin">
+          <Link to="/auth/register">
             <button> Sing Up</button>
           </Link>
         </div>
         <div className="applicationImg">
-          <img src="/apka.jpeg" alt=""></img>
+          <img src="/apka2.jpeg" alt=""></img>
+          {/* <img src="/dashboardApp.png" alt=""></img> */}
+        </div>
+        <div className="applicationText">
+          <h2>Speed up your growth by using this tools:</h2>
         </div>
         <CardSlider />
+      </div>
+      <div className="contact">
+        <div className="contactDetails">
+          <p>Wealth Voyage</p>
+          <p>e-mail: wealth.voyage@gmail.com</p>
+          <p>phone: 123456789</p>
+        </div>
+        <div className="contactLogin">
+          <Link to="/auth/signin">Sign In</Link>
+          <hr/>
+          <Link to="/auth/register">Sign up</Link>
+        </div>
       </div>
       <Footer />
     </div>
