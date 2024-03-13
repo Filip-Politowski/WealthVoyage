b8afe06f9da3cb259f3b51fc86cd8f0ac4f81fca
Filip Politowski <fil.politowski@gmail.com>
2024-03-14 00:00:12 +0100

    New way to use JWT tokens

--- WealthVoyage-backend-01/src/main/java/pl/savings/wealthvoyage/controller/AccountController.java
+++ WealthVoyage-backend-01/src/main/java/pl/savings/wealthvoyage/controller/AccountController.java
@@ -2,12 +2,16 @@ package pl.savings.wealthvoyage.controller;
 
 import lombok.Data;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin("http://localhost:3000")
 @RestController
-@RequestMapping("/api/accounts")
-@Data
+@RequestMapping("/api/account")
 public class AccountController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
 }
