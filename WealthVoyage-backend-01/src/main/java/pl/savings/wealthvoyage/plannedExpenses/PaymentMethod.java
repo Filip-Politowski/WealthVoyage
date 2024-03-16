09a348e338f44e60506bb8d86b336c58a341a922
Filip Politowski <fil.politowski@gmail.com>
2024-03-16 08:54:20 +0100

    Created entity plannedExpense

---
+++ WealthVoyage-backend-01/src/main/java/pl/savings/wealthvoyage/plannedExpenses/PaymentMethod.java
@@ -0,0 +1,9 @@
+package pl.savings.wealthvoyage.plannedExpenses;
+
+public enum PaymentMethod {
+    CARD,
+    CASH,
+    BANK_TRANSFER,
+    PAYPAL,
+    OTHER;
+}
---
+++ WealthVoyage-backend-01/src/main/java/pl/savings/wealthvoyage/plannedExpenses/Status.java
@@ -0,0 +1,6 @@
+package pl.savings.wealthvoyage.plannedExpenses;
+
+public enum Status {
+    PAID,
+    PAYABLE
+}
