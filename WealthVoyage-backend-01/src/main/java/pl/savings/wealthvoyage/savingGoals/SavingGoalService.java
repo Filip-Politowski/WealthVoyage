50aa160eeea0f0ec144839c60ef943f6feb09cda
Filip Politowski <fil.politowski@gmail.com>
2024-04-04 20:52:05 +0200

    Implemented simple service to savingGoals

---
+++ WealthVoyage-backend-01/src/main/java/pl/savings/wealthvoyage/savingGoals/SavingGoalService.java
@@ -0,0 +1,43 @@
+package pl.savings.wealthvoyage.savingGoals;
+
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import pl.savings.wealthvoyage.user.User;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+@RequiredArgsConstructor
+public class SavingGoalService {
+    private final SavingGoalRepository savingGoalRepository;
+    private final SavingGoalMapper savingGoalMapper;
+
+    public SavingGoalResponse getUserSavingGoalById(Long id, @NotNull UserDetails userDetails){
+        return savingGoalMapper.toSavingGoalResponse(savingGoalRepository.findByIdAndUsername(id, userDetails.getUsername()).orElseThrow(NoSuchElementException::new));
+    }
+
+    public List<SavingGoalResponse> getUserSavingGoals(@NotNull UserDetails userDetails){
+        return savingGoalMapper.toSavingGoalResponseList(savingGoalRepository.findAllByUsername(userDetails.getUsername()).orElseThrow(NoSuchElementException::new));
+    }
+    public SavingGoal saveUserSavingGoal(SavingGoalRequest savingGoalRequest, @NotNull UserDetails userDetails){
+        SavingGoal savingGoal = savingGoalMapper.toSavingGoal(savingGoalRequest);
+        savingGoal.setUsername(userDetails.getUsername());
+        return savingGoalRepository.save(savingGoal);
+    }
+    public void deleteUserSavingGoalById(Long id, @NotNull UserDetails userDetails){
+        savingGoalRepository.deleteByIdAndUsername(id, userDetails.getUsername());
+    }
+    public void updateUserSavingGoal(Long id, SavingGoalRequest savingGoalRequest, @NotNull UserDetails userDetails){
+        SavingGoal savingGoal = savingGoalMapper.toSavingGoal(savingGoalRequest);
+        savingGoal.setUsername(userDetails.getUsername());
+        savingGoal.setId(id);
+        savingGoalRepository.save(savingGoal);
+    }
+
+    public Double getUserSavingGoalSum(@NotNull UserDetails userDetails){
+        return savingGoalRepository.findAllByUsername(userDetails.getUsername()).orElseThrow(NoSuchElementException::new).stream().mapToDouble(SavingGoal::getAmountSaved).sum();
+    }
+}
