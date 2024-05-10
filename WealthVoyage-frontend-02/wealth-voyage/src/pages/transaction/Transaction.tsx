d12423e97f955945bcc1611de9123912d7653a2a
Filip Politowski <fil.politowski@gmail.com>
2024-05-10 19:20:29 +0200

    Changes in transaction, deleted component singleViewTransaction

--- WealthVoyage-frontend-02/wealth-voyage/src/pages/transaction/Transaction.tsx
+++ WealthVoyage-frontend-02/wealth-voyage/src/pages/transaction/Transaction.tsx
@@ -1,13 +1,48 @@
-import { singleTransaction } from "../../data";
-import "./transaction.scss";
-import TransactionSingleView from "../../components/transactionSingleView/TransactionSingleView";
 
-const transaction = () => {
+import "./transaction.scss";
+
+import { useState } from "react";
+
+
+
+const Transaction = () => {
+
+  const [transaction, setTransaction] = useState();
+
   return (
     <div className="transaction">
-      <TransactionSingleView transaction={singleTransaction}/>
+      <div className="topInfo">
+        <h1>Transaction Details</h1>
+        <img src="/transaction2.svg" alt="" />
+      </div>
+      <div className="transactionDetails">
+        {transaction &&
+          Object.entries(transaction)
+            .filter((item) => item[0] !== "id")
+            .map((item) => (
+              <div className="row">
+                <div className="rowDetails">
+                  <p>
+                    {item[0][0].toUpperCase() +
+                      item[0]
+                        .slice(1)
+                        .replace(/([A-Z])/g, " $1")
+                        .toLowerCase()}
+                    :
+                  </p>
+
+                
+                </div>
+                <hr />
+              </div>
+            ))}
+      </div>
+      <div className="buttonsSection">
+        <button>Delete</button>
+        <button>Update</button>
+      </div>
     </div>
   );
 };
 
-export default transaction;
+export default Transaction;
